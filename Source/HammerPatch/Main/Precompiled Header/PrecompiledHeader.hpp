651e82ce8be1908a93a751047dec9b96d395b256
crashfort <crashfort@users.noreply.github.com>
2017-05-08 19:32:12 +0200

    Include some Windows libraries

--- Source/HammerPatch/Main/Precompiled Header/PrecompiledHeader.hpp
+++ Source/HammerPatch/Main/Precompiled Header/PrecompiledHeader.hpp
@@ -3,12 +3,16 @@
 #include "TargetVersion.hpp"
 
 #include <windows.h>
+#include <Shlwapi.h>
 #include <Psapi.h>
+
+#include <wrl.h>
 
 #include <vector>
 #include <memory>
 #include <thread>
 #include <chrono>
+#include <initializer_list>
 #include <stdint.h>
 
 #include "3rd Party Libraries\MinHookCPP.hpp"
